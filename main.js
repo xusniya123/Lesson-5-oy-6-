f940252f951177b1299702f8e12035d6f0c5a632
xusniya123 <xusniya12345678@gmail.com>
2024-04-22 17:02:41 +0500

    Lesson 5 moth (6)

---
+++ main.js
@@ -0,0 +1,18 @@
+// const elements = document.getElementById("title");
+// elements.innerHTML = "BMW"
+// const element = document.getElementsByClassName("main");
+// element[0].innerHTML = "2016 yilda yetakchi nemis avtokonsernlaridan biri, BMW (nem. Bayerische Motoren Werke-Bavariya motor zavodi) o‘zining yuz yilligini nishonlamoqda."
+// const element  = document.getElementsByTagName("style");
+// element.innerHTML = "Aytish joizki, bu eng birinchi avtomobil zavodi emas, Mercedes-Benz, Opel, Ford, Renault va Fiat kompaniyalari undan avval paydo bo‘lgan, ammo BMW o‘z sinfining yo‘nalishini belgilab beruvchi avtomobillarni ko‘plab ishlab chiqara olgan, desak xato bo‘lmaydi."
+// const element  = document.querySelectorAll("btn");
+// element.innerHTML = "btn"
+let text = document.getElementById('text');
+let text_background = prompt("Enter body backraundColor")
+let text_color = prompt("Enter text color")
+
+
+text.style.backgroundColor = text_background
+text.style.color = text_color
+
+
+
